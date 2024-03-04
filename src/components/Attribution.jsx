84011f75ed2c5f7f56e530f38f0ee7dc678ae11c
Ben Akram <105305419+benakram4@users.noreply.github.com>
2024-03-04 15:08:46 -0500

    refactor: :lipstick: Update font sizes and text colors in components, exported testimonials list to a separate page

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -8,12 +8,12 @@ export default function Attribution({
   ...props
 }) {
   return (
-    <p className="text-[10px] leading-none pl-2 pt-2" {...props}>
+    <p className="text-[9px] sm:text-[10px] leading-none pl-2 pt-2" {...props}>
       {type} made by
       <Link
         target="_blank"
         href={attributeSource}
-        className={`${buttonVariants({ variant: 'link', size: 'xxs', className: 'text-[9px] leading-none pl-1' })} `}
+        className={`${buttonVariants({ variant: 'link', size: 'xxs', className: 'text-[8px] leading-none pl-1' })} `}
       >
         {author}
       </Link>

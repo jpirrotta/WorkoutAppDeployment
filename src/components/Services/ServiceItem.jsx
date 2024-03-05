3c944010823b74adf4fae96ed67b515e8c937a15
Jesse Pirrotta <79476546+jpirrotta@users.noreply.github.com>
2024-03-04 22:09:34 -0500

    Update ServiceItem.jsx

--- src/components/Services/ServiceItem.jsx
+++ src/components/Services/ServiceItem.jsx
@@ -1,4 +1,4 @@
-import { buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/Button';
 import Link from 'next/link';
 import Attribution from '@/components/Attribution.jsx';
 

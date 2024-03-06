c968069098671d357d2146215eafe01898d40164
Jesse Pirrotta <jessepirrotta@gmail.com>
2024-03-06 11:55:22 -0500

    changed button varient

--- src/app/exercises/page.jsx
+++ src/app/exercises/page.jsx
@@ -5,6 +5,7 @@ import ExerciseCards from '@/components/ExerciseCards';
 import { ExerciseOptions, fetchData } from '@/utils/fetchData';
 import useSWR from 'swr';
 import Spinner from '@/components/svgs/Spinner.svg';
+import { Button } from '@/components/ui/Button';
 
 export default function ExercisePage() {
   const [limit, setLimit] = useState(6);
@@ -28,14 +29,18 @@ export default function ExercisePage() {
     );
 
   return (
-    <div className="bg-slate-900 min-h-screen p-4">
+    <div className="bg-slate-900 min-h-screen p-4 flex flex-col justify-between">
       <h1 className="text-primary text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl mb-4">
         Exercises
       </h1>
       <ExerciseCards exercises={exercises} />
-      <button onClick={loadMore} className="mt-4">
-        Load More
-      </button>
+      <Button
+        className="px-0 bottom-0 left-0 right-0 flex items-center justify-center"
+        variant="link"
+        onClick={loadMore}
+      >
+        Show More
+      </Button>
     </div>
   );
 }

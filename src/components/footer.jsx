5dc1b863c69f66e1a67983067f6364b47b44ca70
kvora2 <kelvinvora21@gmail.com>
2024-03-12 15:16:45 -0400

    fix: :bug: Fix footer alignment and svg logos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ import Youtube from '@/components/svgs/Youtube.svg';
 import Instagram from '@/components/svgs/Instagram.svg';
 
 const StyledIcon = ({ Icon, ...props }) => {
-    return <Icon width={'150%'} {...props} />;
+    return <Icon width={'130%'} {...props} />;
 };
 
 export default function Footer() {
@@ -35,8 +35,8 @@ export default function Footer() {
     ]
     return (
         <div className="bg-rose-600">
-            <div className="max-w-screen-lg py-8 px-4 sm:px-6 text-gray-100 sm:flex justify-between mx-auto">
-                <div className="p-5 text-center sm:w-2/12 border-r border-indigo-950 sm:text-left">
+            <div className="max-w-screen-lg py-8 px-4 sm:px-6 text-gray-100 sm:flex mx-auto justify-center items-center">
+                <div className="p-5 text-center sm:w-1/6 border-r border-indigo-950 sm:text-left">
                     <div className="uppercase text-indigo-950 font-bold">Menu</div>
                     <ul>
                         {links.map(({ id, title, link }) => (
@@ -51,15 +51,15 @@ export default function Footer() {
                         ))}
                     </ul>
                 </div>
-                <div className="p-5 text-center sm:w-7/12 border-r border-indigo-950">
+                <div className="p-5 text-center sm:w-4/6 border-r border-indigo-950">
                     <h3 className="uppercase font-bold text-xl text-indigo-950 mb-4">Componentity</h3>
                     <p className="text-gray-100 text-sm mb-10">Thinking of adding a Input area for User{'\''}s Email Address to provide them daily updates and other news</p>
                 </div>
-                <div className="text-center sm:text-left p-5 sm:w-3/12">
+                <div className="text-center sm:text-left p-5 sm:w-1/6">
                     <div className="uppercase text-indigo-950 font-bold">Contact Us</div>
                     <ul>
                         <li className="my-2">
-                            <a className="hover:text-gray-300" href="#">XXX XXXX, Seneca Newnham, North York</a>
+                            <p>XXX XXXX, Seneca Newnham, North York</p>
                         </li>
                         <li className="my-2">
                             <a className="hover:text-gray-300" href="#">contact@company.com</a>
@@ -69,36 +69,35 @@ export default function Footer() {
             </div>
             <div className="flex py-3 m-auto text-gray-800 text-sm flex-col items-center border-t max-w-screen-xl border-indigo-950">
                 <div className="md:flex-auto md:flex-row mt-2 flex-row flex">
-                    <Link href="#" className="w-6 mx-6">
+                    <Link href="#" className="w-6 mx-6 text-indigo-950 hover:text-gray-300 hover:h-7 hover:w-7 duration-200">
                         <StyledIcon
                             Icon={Twitter}
                         />
                     </Link>
 
-                    <Link href="#" className="w-6 mx-6">
+                    <Link href="#" className="w-6 mx-6 text-indigo-950 hover:text-gray-300 hover:h-7 hover:w-7 duration-200">
                         <StyledIcon
                             Icon={Facebook}
                         />
                     </Link>
 
-                    <Link href="#" className="w-6 mx-6">
+                    <Link href="#" className="w-6 mx-6 text-indigo-950 hover:text-gray-300 hover:h-7 hover:w-7 duration-200">
                         <StyledIcon
                             Icon={Linkedin}
                         />
                     </Link>
 
-                    <Link href="#" className="w-6 mx-6">
+                    <Link href="#" className="w-6 mx-6 text-indigo-950 hover:text-gray-300 hover:h-7 hover:w-7 duration-200">
                         <StyledIcon
                             Icon={Youtube}
                         />
                     </Link>
 
-                    <Link href="#" className="w-6 mx-6">
+                    <Link href="#" className="w-6 mx-6 text-indigo-950 hover:text-gray-300 hover:h-7 hover:w-7 duration-200">
                         <StyledIcon
                             Icon={Instagram}
                         />
                     </Link>
-                    {/* Insert other social media icons similarly */}
                 </div>
                 <div className="my-5 text-gray-100">© Copyright 2024. All Rights Reserved.</div>
             </div>

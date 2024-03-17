746b0ae56d2fea084494c143185fb199be2bdf6c
Ben Akram <105305419+benakram4@users.noreply.github.com>
2024-03-17 15:54:50 -0400

    refactor: Refactor DeleteProfileData component to handle loading state. improved handle delete

--- src/components/Profile/DeleteProfileData.jsx
+++ src/components/Profile/DeleteProfileData.jsx
@@ -1,6 +1,9 @@
+// src/components/Profile/DeleteProfileData.jsx
+'use client';
 import logger from '@/lib/logger.js';
-import { Button } from '@/components/ui/Button';
 
+// ui components
+import { Button } from '@/components/ui/Button';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -12,25 +15,45 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/Alert-Dialog';
+//
 
-const handleDeleteProfile = async (id) => {
-  logger.info('Deleting profile data');
-  try {
-    const res = await fetch(`/api/profile?userId=${id}`, {
-      method: 'DELETE',
-    });
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-    logger.info('Profile data deleted');
-  } catch (error) {
-    logger.error(error);
-  }
-};
+// states
+import { useAtom, atom } from 'jotai';
+import { profileDataAtom } from '../../../store.js';
 
+const loadingAtom = atom(false);
 export default function DeleteProfileData({ userId }) {
+  const [, setProfileData] = useAtom(profileDataAtom);
+  const [loading, setLoading] = useAtom(loadingAtom); // Add this line
+
+  const handleDeleteProfile = async (id) => {
+    setLoading(true); // Set loading to true when the operation starts
+    logger.info('Deleting profile data');
+    try {
+      const res = await fetch(`/api/profile?userId=${id}`, {
+        method: 'DELETE',
+      });
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      // Clear profile data from the store
+      setProfileData(null);
+      // refresh the form page
+      // window.location.reload();
+      //
+      logger.info('Profile data deleted');
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false); // Set loading to false when the operation finishes
+    }
+  };
+  // Display a loading spinner when the operation is in progress
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row pt-4 justify-between">
       <div className="flex flex-col">
         <p className="font-medium">Delete Profile</p>
         <p className="text-sm text-muted-foreground">

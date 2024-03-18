d9285a7d71bddc6997b94cfd85cb09d904940c99
Luca Casola <lcasola@myseneca.ca>
2024-03-18 06:48:50 -0400

    Implemented email notifications for form submissions
    Added functionality to send an email containing form fields upon form submission. Utilized email service API for seamless integration and enhanced user experience.

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,3 +1,5 @@
+import emailjs from '@emailjs/browser';
+
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -5,27 +7,13 @@ import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/Select';
-
-import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs';
-
 
 const contactFormSchema = z.object({
   name: z.string(),
@@ -38,25 +26,28 @@ const contactFormSchema = z.object({
 export default function ContactForm() {
   // define the form
   const form = useForm({
-    resolver: zodResolver(contactFormSchema),
-    defaultValues: {
-      name: 'John Doe',
-      email: 'johnDoe@mail.com',
-      subject: 'enter message subject here',
-      message: 'enter message here',
-    },
+    resolver: zodResolver(contactFormSchema)
   });
 
 
-  // handle form submission
-  const onSubmit = (data) => {
-    console.log("form submitted")
-    console.log(data);
-  };
+  // send email with user inputted data using emailjs
+  const serviceID = 'service_p2hlq4k'
+  const testmplateID = 'template_contactRequest'
+  const publicKey = 'mU9AJxqdfYVzs73wC'
+
+  const onSubmit = (emailData) => {
+    emailjs.send(serviceID, testmplateID, emailData, publicKey)
+      .then(() => {
+        console.log('SUCCESS!');
+      })
+      .catch((error) => {
+        console.log('FAILED...', error.text);
+      })
+  }
 
 
   return (
-    <section className="flex flex-col w-full">
+    <section className="flex flex-col w-full text-primary-foreground">
       <h2 className="pb-2 text-4xl font-bold italic text-primary">Contact Request</h2>
       <Form {...form} className="flex justify-center">
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -65,12 +56,12 @@ export default function ContactForm() {
             <div className="flex-grow">
               <FormField
                 control={form.control}
-                name="contact.name"
+                name="name"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input type="string" {...field} />
+                      <Input type="string" placeholder="Enter your name" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -80,12 +71,12 @@ export default function ContactForm() {
             <div className="flex-grow">
               <FormField
                 control={form.control}
-                name="contact.email"
+                name="email"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input type="string" {...field} />
+                      <Input type="string" placeholder="example@gmail.com" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -96,12 +87,12 @@ export default function ContactForm() {
 
           <FormField
             control={form.control}
-            name="contact.subject"
+            name="subject"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Subject</FormLabel>
                 <FormControl>
-                  <Input type="string" {...field} />
+                  <Input type="string" placeholder="message subject" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -110,19 +101,19 @@ export default function ContactForm() {
 
           <FormField
             control={form.control}
-            name="contact.message"
+            name="message"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Message</FormLabel>
                 <FormControl>
-                  <Input type="string" {...field} />
+                  <Input type="text" multiline={true} className="text-left h-40 w-full" placeholder="message" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-          <div className="flex justify-center md:justify-start">
+          <div className="flex justify-center">
             <Button type="submit">Submit</Button>
           </div>
         </form>

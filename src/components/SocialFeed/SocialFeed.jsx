a04811f8819e6547b1f87c55aee061baed732bee
Luca Casola <lcasola@myseneca.ca>
2024-03-31 15:38:36 -0400

    updated social feed and social workout card

--- src/components/SocialFeed/SocialFeed.jsx
+++ src/components/SocialFeed/SocialFeed.jsx
@@ -1,12 +1,46 @@
 'use client';
 
+import React, { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/Button';
 import SocialWorkoutCard from '@/components/SocialFeed/SocialWorkoutCard';
 
 export default function SocialFeed() {
-    return (
-      <div> 
-        <h1>Social Feed</h1>
-      </div>
-    );
+  const [workouts, setWorkouts] = useState([]);
+  const [itemsPerPage, setItemsPerPage] = useState(1);
+
+  const handleGetAllPublicWorkouts = async (page, itemsPerPage) => {
+    console.log('Get All Public Workouts');
+    const response = await fetch(`/api/public-workouts?page=${page}&itemsPerPage=${itemsPerPage}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    setWorkouts(data);
   }
+
+  useEffect(() => {
+    handleGetAllPublicWorkouts(1, itemsPerPage);
+  }, []);
+
+  const loadMore = () => {
+    setItemsPerPage(itemsPerPage + 1);
+    handleGetAllPublicWorkouts(1, itemsPerPage);
+  };
+
+  return (
+    <div className="flex flex-col space-y-8 p-20 items-center justify-center">
+      {workouts.map((workout, index) => (
+        <SocialWorkoutCard key={index} workout={workout} />
+      ))}
+      <Button
+        className=""
+        onClick={loadMore}
+      >
+        Show More
+      </Button>
+    </div>
+  );
+}
   
--- src/components/SocialFeed/SocialWorkoutCard.jsx
+++ src/components/SocialFeed/SocialWorkoutCard.jsx
@@ -1,6 +1,105 @@
+'use client';
+import { Button } from '@/components/ui/Button';
+import { useUser } from '@clerk/clerk-react';
 
-export default function SocialWorkoutCard() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/Card';
+
+export default function SocialWorkoutCard({ workout }) {
+    // use the useUser hook to get the current user
+    const { user } = useUser();
+
+    // critical default values
+    const userId = user?.id;
+    const userName = user?.fullName;
+    const BodyData = {
+      userId: userId,
+      name: userName,
+      workout: workout,
+    };
+  
+    const handleLikeWorkout = async () => {
+      console.log('Like Workout');
+      const response = await fetch('/api/workout/like', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId: userId, workoutId: testWorkoutId }),
+      });
+    };
+  
+    const handleUnlikeWorkout = async () => {
+      console.log('Unlike Workout');
+      const response = await fetch('/api/workout/like', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId: userId, workoutId: testWorkoutId }),
+      });
+    };
+  
+    const handleCommentWorkout = async () => {
+      console.log('Comment Workout');
+      const response = await fetch('/api/workout/comment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: userId,
+          comment: testComment,
+          workoutId: testWorkoutId,
+        }),
+      });
+    };
+  
+    const handleDeleteComment = async () => {
+      console.log('Delete Comment');
+      const response = await fetch('/api/workout/comment', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: userId,
+          commentId: testCommentId,
+          workoutId: testWorkoutId,
+        }),
+      });
+    };
+
+
   return (
-    <h1>social workout card</h1>
+      <Card
+        className="w-1/3 bg-slate-700 border-primary md:transform  md:transition-transform md:duration-200"
+      >
+        <CardHeader>
+          <CardTitle className="text-secondary uppercase text-center">
+            {workout.name}
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <CardDescription className="text-secondary capitalize">
+            <h1>Description</h1>
+          </CardDescription>
+
+
+        </CardContent>
+
+        <CardFooter className="capitalize text-secondary">
+          <Button className="mr-4">
+            Like
+          </Button>
+        </CardFooter>
+      </Card>
   );
 }

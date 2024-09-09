b74e8b94405f7cf9240c69ea158ae879f5459dfa
Ben Akram <105305419+benakram4@users.noreply.github.com>
2024-09-08 23:42:10 -0400

    refactor: created a component that holds out landing page and render it at the home page, instead of rending it as our home page. this will be useful later when we will render the dashboard for existing users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-'use client';
-
-import Hero from '@/components/Hero';
-import Services from '@/components/services/Services';
-import CallToActionSection from '@/components/CallToActionSection';
-import Testimonials from '@/components/Testimonials/Testimonials';
+import LandingPage from '@/components/LandingPage';
 
 export default function Home() {
-
   return (
     <main className="flex flex-col items-center justify-between">
-      <Hero />
-      <Services />
-      <CallToActionSection
-        title="Ready To Start Your Fitness Journey ?"
-        text="Join our community and start tracking your workouts, sharing your routines, and learning together."
-        buttonText="Sign Up"
-        buttonLink="/sign-up"
-      />
-      <Testimonials />
+      <LandingPage />
     </main>
   );
 }

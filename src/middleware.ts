ef8ec4ab70974211754b592505aa9eb14c6fbb76
Ben Akram <bakram4@myseneca.ca>
2024-09-20 18:53:32 -0400

    chore: deleted unused route feat: added route and api end points protection(auth check)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,18 @@
-
-import {
-  clerkMiddleware,
-  createRouteMatcher,
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import logger from '@/lib/logger';
 
 // Log the log level
 logger.info(`initializing clerkMiddleware`);
 
-const isProtectedRoute = createRouteMatcher(['/about-us']);
+const isProtectedRoute = createRouteMatcher([
+  '/account(/.*)?',
+  '/calculators(/.*)?',
+  '/dashboard',
+  '/feed',
+  '/profile',
+  '/workouts(/.*)?',
+  '/(api|trpc)(.*)',
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (!auth().userId && isProtectedRoute(req)) {
@@ -18,11 +22,3 @@ export default clerkMiddleware((auth, req) => {
   }
   console.log('User is signed in');
 });
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
